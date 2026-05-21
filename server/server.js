f6711bdfdf075f1536d9219eb010fcd80c12971b
Aakanksha_404NotFound <aakankshamore2805@gmail.com>
2026-05-21 17:14:37 +0530

    fix: remove duplicate PORT declaration causing SyntaxError on startup

--- server/server.js
+++ server/server.js
@@ -132,7 +132,6 @@ const PORT = process.env.PORT || 5000;
 setInterval(cleanupExpiredOtps, 60 * 1000);
 cleanupExpiredOtps();
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 
